264a8d58febed05e0e91f6723c051a8e244ccf1e
anwar-duc <anwarboumnijel63@gmail.com>
2024-11-21 21:38:48 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Experience } from './components/Experience';
 import { Skills } from './components/Skills';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import { WhatIDo } from './components/WhatIDo';  // Import WhatIDo component
+import { WhatIDo } from './components/WhatIDo';
 import { Education } from './components/Education';  // Import Education component
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Hero />
       <WhatIDo />  {/* Add WhatIDo component here */}
       <Experience />
-      <Education />  {/* Add Education section here */}
+      <Education /> {/* Add Education component here */}
       <Skills />
       <Contact />
       <Footer />
