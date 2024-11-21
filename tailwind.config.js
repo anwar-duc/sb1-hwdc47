1b2e06afdd6228e1def516cdd67b000e44953d6b
anwar-duc <anwarboumnijel63@gmail.com>
2024-11-21 20:05:07 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in-slide-up': 'fadeInSlideUp 0.6s ease-out forwards', // Animation for fading in and sliding up
+      },
+      keyframes: {
+        fadeInSlideUp: {
+          '0%': { opacity: '0', transform: 'translateY(30px)' }, // Start: invisible and slide from below
+          '100%': { opacity: '1', transform: 'translateY(0)' }, // End: fully visible and in place
+        },
+      },
+    },
   },
   plugins: [],
 };
