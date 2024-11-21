090c4c75b02b7f794065e5338424abc3493e1050
anwar-duc <anwarboumnijel63@gmail.com>
2024-11-21 20:53:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Experience } from './components/Experience';
 import { Skills } from './components/Skills';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { WhatIDo } from './components/WhatIDo';  // Import WhatIDo component
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Hero />
+      <WhatIDo />  {/* Add WhatIDo component here */}
       <Experience />
       <Skills />
       <Contact />
