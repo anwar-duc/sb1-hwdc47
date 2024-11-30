27b11f00eb973e7bb21bfa1ba856315c8508abe7
anwar-duc <anwarboumnijel63@gmail.com>
2024-11-30 17:50:22 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,16 @@ import cvFile from '../assets/cv.pdf'; // Ensure CV file path is correct
 export const Hero = () => {
   return (
     <div className="relative bg-white text-gray-800 dark:bg-gray-900 dark:text-white">
-      <div className="container mx-auto px-6 py-28 flex flex-col md:flex-row items-center justify-between">
+      <div className="container mx-auto px-6 py-20 md:py-28 flex flex-col md:flex-row items-center justify-between">
         {/* Left Column (Text and Actions) */}
         <div className="md:w-1/2 mb-8 md:mb-0 text-center md:text-left">
-          <h1 className="text-5xl md:text-6xl font-bold mb-6 text-teal-600 dark:text-teal-400">Adem Bouteraa</h1>
-          <h2 className="text-xl md:text-2xl mb-4 text-gray-600 dark:text-gray-300">Business Intelligence Professional</h2>
-          <p className="text-lg md:text-xl mb-8 text-gray-700 dark:text-gray-400">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-6 text-teal-600 dark:text-teal-400">
+            Adem Bouteraa
+          </h1>
+          <h2 className="text-lg sm:text-xl md:text-2xl mb-4 text-gray-600 dark:text-gray-300">
+            Business Intelligence Professional
+          </h2>
+          <p className="text-base sm:text-lg md:text-xl mb-8 text-gray-700 dark:text-gray-400">
             Data-driven professional with expertise in using business intelligence tools like Python, Power BI, and SQL to drive insights and growth.
           </p>
           <div className="flex justify-center md:justify-start space-x-6">
@@ -41,11 +45,11 @@ export const Hero = () => {
         </div>
 
         {/* Right Column (Profile Image) */}
-        <div className="md:w-1/3">
+        <div className="md:w-1/3 sm:w-2/3">
           <img
             src={profilePic}
             alt="Adem Bouteraa"
-            className="rounded-full w-72 h-72 object-cover shadow-xl border-8 border-white dark:border-gray-700"
+            className="rounded-full w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96 object-cover shadow-xl border-8 border-white dark:border-gray-700"
           />
         </div>
       </div>
