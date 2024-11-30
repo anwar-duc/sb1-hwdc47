91a4f759189164a7375da3ad973ab6186b0db36c
anwar-duc <anwarboumnijel63@gmail.com>
2024-11-30 14:57:20 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,9 @@ export const Footer = () => {
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-6 text-center">
         <p>© 2024 Adem Bouteraa. All rights reserved.</p>
+        <p>
+          Designed with ❤️ by <span className="font-bold">Adem Bouteraa</span>.
+        </p>
       </div>
     </footer>
   );
